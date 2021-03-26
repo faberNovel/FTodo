9ce7ee9c6c78612659b01909cb92b03a086aa0a2
Denis Poifol <denis.poifol@gmail.com>
2021-03-26 16:03:12 +0100

    Initial Commit

---
+++ FTODO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FTODO
+//
+//  Created by Denis Poifol on 26/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FTODO/FTODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  FTODOApp.swift
+//  FTODO
+//
+//  Created by Denis Poifol on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct FTODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

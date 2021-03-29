2b9956a9e9c271846497b62a2a800934615cb894
Denis Poifol <denis.poifol@gmail.com>
2021-03-29 16:28:31 +0200

    Add window content to FTodoApp

--- FTODO/ContentView.swift
+++ FTODO/ContentView.swift
@@ -9,8 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            Text("application_title")
+                .font(.largeTitle)
+                .bold()
+                .padding(.leading, 16)
+            Text("application_description")
+                .lineLimit(nil)
+            Spacer().frame(height: 16)
+            Group {
+                Text("installing_instructions_title")
+                    .font(.title3)
+                    .bold()
+                    .padding(.leading, 16)
+                Text("installing_instructions_steps")
+            }
+            Spacer().frame(height: 16)
+            Group {
+                Text("use_instructions_title")
+                    .font(.title3)
+                    .bold()
+                    .padding(.leading, 16)
+                Text("use_instructions_detail")
+                Text("todo_command_instruction_title")
+                    .underline()
+                    .padding(.leading, 8)
+                Text("todo_command_description")
+                Text("fixme_command_instruction_title")
+                    .underline()
+                    .padding(.leading, 8)
+                Text("fixme_command_description")
+                Text("comment_command_instruction_title")
+                    .underline()
+                    .padding(.leading, 8)
+                Text("comment_command_description")
+            }
+            Spacer().frame(height: 16)
+            Text("general_commands_detail")
+        }
+        .padding()
+        .frame(width: 415, height: 440)
     }
 }
 

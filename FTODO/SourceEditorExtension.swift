05e7e5783cc2c2bc35e5536274c6417cc029970a
Denis Poifol <denis.poifol@gmail.com>
2021-03-29 08:15:42 +0200

    Create Xcode source editor extension

---
+++ FTODO/SourceEditorExtension.swift
@@ -0,0 +1,26 @@
+//
+//  SourceEditorExtension.swift
+//  FTodo
+//
+//  Created by Denis Poifol on 29/03/2021.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorExtension: NSObject, XCSourceEditorExtension {
+    
+    /*
+    func extensionDidFinishLaunching() {
+        // If your extension needs to do any work at launch, implement this optional method.
+    }
+    */
+    
+    /*
+    var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
+        // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
+        return []
+    }
+    */
+    
+}
